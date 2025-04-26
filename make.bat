cb87dcb07e5f9ee357ae9832ce04a5daf8e0482c
DevonArtmeier <devon.artmeier@outlook.com>
2025-04-26 14:37:41 -0400

    Rename source folder to src

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo off
 if not exist build mkdir build
-cd source
+cd src
 ..\tools\asm68k /k /p /o ae- sonic.asm, ../build/gametap.gen, , ../build/sonic.lst
 ..\tools\fixheadr ../build/gametap.gen
 pause
