a5e07ed8c8bbe8eb3c3c4013f3fd80efec4cda78
DevonArtmeier <devon.artmeier@outlook.com>
2025-04-26 14:53:07 -0400

    Rename tools folder to bin

--- make.bat
+++ make.bat
@@ -1,6 +1,6 @@
 @echo off
 if not exist build mkdir build
 cd src
-..\tools\asm68k /k /p /o ae- sonic.asm, ../build/gametap.gen, , ../build/sonic.lst
-..\tools\fixheadr ../build/gametap.gen
+..\bin\asm68k /k /p /o ae- sonic.asm, ../build/gametap.gen, , ../build/sonic.lst
+..\bin\fixheadr ../build/gametap.gen
 pause
